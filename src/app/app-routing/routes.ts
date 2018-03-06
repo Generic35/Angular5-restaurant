f56a64584c36ffd36733b241c559e3e29b01e2da
yulia <yulia@Yulias-iMac.local>
2018-03-05 21:24:51 -0500

    Add Routing to the dish details to display it as a separate page

--- src/app/app-routing/routes.ts
+++ src/app/app-routing/routes.ts
@@ -9,6 +9,7 @@ import { DishdetailComponent } from '../dishdetail/dishdetail.component';
 export const routes: Routes = [
   { path: 'home',       component: HomeComponent },
   { path: 'menu',       component: MenuComponent },
+  { path: 'dishdetail/:id',     component: DishdetailComponent },
   { path: 'contactus',  component: ContactComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' }
 ];
--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Dish } from '../shared/dish';
 
+import { DishService } from '../services/dish.service';
+
+import { Params, ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+
 @Component({
   selector: 'app-dishdetail',
   templateUrl: './dishdetail.component.html',
@@ -8,12 +13,19 @@ import { Dish } from '../shared/dish';
 })
 
 export class DishdetailComponent implements OnInit {
-  @Input()
+  // @Input()
   dish: Dish;
 
-  constructor() { }
+  constructor(private dishservice: DishService,
+              private route: ActivatedRoute,
+              private location: Location) { }
 
   ngOnInit() {
+    const id = +this.route.snapshot.params['id'];
+    this.dish = this.dishservice.getDish(id);
   }
 
+  goBack(): void {
+    this.location.back();
+  }
 }
