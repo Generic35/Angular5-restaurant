cf3a464d861728c0a2f97f9c2a191162735f6fa7
yuliamsky <julia_moldavsky@yahoo.com>
2018-03-19 14:15:16 -0400

    build

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -8,7 +8,7 @@ import { MdDialog, MdDialogRef } from '@angular/material';
 })
 export class LoginComponent implements OnInit {
 
-  user = { remember: false };
+  user = {username: '', password: '', remember: false};
   constructor(public dialogRef: MdDialogRef<LoginComponent>) { }
 
   ngOnInit() {
